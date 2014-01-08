8fd68f77b7facfa09f443a3bd6442e3e2d75020f
zhoumenglin <allen@qiaobutang.com>
2014-01-08 18:10:14 +0800

    practice the use of jsoup

--- src/main/scala/net/liftweb/example/snippet/Scraper.scala
+++ src/main/scala/net/liftweb/example/snippet/Scraper.scala
@@ -4,11 +4,29 @@ import scala.collection.JavaConversions.asScalaBuffer
 import net.liftweb.http.DispatchSnippet
 import scala.xml.NodeSeq
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 
 object Scraper extends DispatchSnippet {
 	def dispatch = {
 		case "wiki"  =>  wiki
-		case "htmlParser"  => htmlParser 
+		case "stringToHtml"  => stringToHtml 
+		case "extractAttr"  => extractAttr
+		case "htCity" => htCity
+	}
+
+	val baseUrlOfXJH = "http://xjh.haitou.cc"
+	val defaultTimeout = 5000
+	val cityLoc = ".city_con li a"
+	def buildConnection(baseUrl: String) = {
+		Jsoup.connect(baseUrl).timeout(defaultTimeout).userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0").get
+	} 
+		
+	def getCityNamesAndUrls(ele: Document, cityLoc: String) = ele.select(cityLoc).map{ alink => (alink.text, s"${baseUrlOfXJH}${alink.attr("href")}") }.toList
+	def getUniversityInCity(cityUrl: String) = {
+		val doc = buildConnection(cityUrl)
+		doc.getElementById("univercityBox").getElementsByAttributeValueMatching("name", "univercity").map {
+			university => (university.text, s"${baseUrlOfXJH}${university.select("a").attr("href")}")
+		}.toList
 	}
 
 	def wiki(xhtml: NodeSeq): NodeSeq = {
@@ -27,13 +45,46 @@ object Scraper extends DispatchSnippet {
 		</div>
 	}
 	
-	def htmlParser(xhtml: NodeSeq): NodeSeq = {
+	def stringToHtml(xhtml: NodeSeq): NodeSeq = {
 		val html = "<html><head><title>First parse</title></head><body><p>Parsed HTML into a doc.</p></body></html>"
 		val document = Jsoup.parse(html)
 		<div>
-			<p>String paser</p> ++
 			{ document }
 		</div>
+	}
+
+	def extractAttr(xhtml: NodeSeq): NodeSeq = {
+		val html = "<p>An <a href='http://example.com/'><b>example</b></a> link.</p>";
+		// parse to html
+		val doc = Jsoup.parse(html);
+		val alink = doc.select("a").first
+		println(doc)
+		println("-----a---------")
+		println(doc.select("a").first)
+		// text between <p></p>
+		<div class="extractAttr">
+			<p>{ html }</p>
+			<p>text between: { doc.body.text }</p>
+			<p>link url: { alink.attr("href")   } </p>
+			<p>link text: { alink.text  }</p>
+			<p>linkOuterH: { alink.outerHtml } </p> <!-- <a href='http://example.com/'><b>example</b></a> -->
+			<p>linkHtml: { alink.html }</p>  <!-- <b>example</b> -->
+		</div>
+	} 
+
+	def htCity(xhtml: NodeSeq): NodeSeq = {
+		getCityNamesAndUrls(buildConnection(baseUrlOfXJH),cityLoc).map { 
+			case (cityText, cityUrl) => {
+				<div>
+					<a href={ cityUrl } target="_blank" class="city">{ cityText }</a>
+					{
+						getUniversityInCity(cityUrl).map { case (universityText, universityUrl) =>
+							<a href={ universityUrl } target="_blank">{ universityText }</a> 
+						}
+					}
+				</div>
+			} 
+		}
 	}
 }
 
