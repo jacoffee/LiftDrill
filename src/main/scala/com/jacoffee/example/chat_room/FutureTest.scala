9c7343a92d6ce639be4ad1995e133d3988f144a0
jacoffee <361541673@163.com>
2014-04-30 23:29:41 +0800

    have a deeper understanding of future but the diffrence of future and actor waiting to be solved

--- src/main/scala/com/jacoffee/example/chat_room/FutureTest.scala
+++ src/main/scala/com/jacoffee/example/chat_room/FutureTest.scala
@@ -1,7 +1,9 @@
 package com.jacoffee.example.chat_room
 
+import org.jsoup.Jsoup
 import scala.concurrent.{ ExecutionContext, Future, future}
 import scala.util.{Try, Success, Failure}
+import scala.collection.JavaConversions.asScalaBuffer
 
 /**
  * Created by qbt-allen on 14-4-30.
@@ -17,8 +19,17 @@ object FutureTest extends App {
 		case msg => println(msg)
 	}
 
-	f.foreach(println)
+	Thread.sleep(1000)
 	// pay attention to here
 	// as the asynchronousm prop, the future will diverge like grapeshot
 	// while the main thread will not wait before the future finish  so the futures are unable to come back  to return before main thread finishes
+
+
+	//   the following two example is to test the essence of Future
+	// concurrently execute some task with returning value(usually, it is the case)
+	// on the bottom is the thread-pool sending/dispatching  thread to finish these task
+	// so to say the basic component finishing task is  Thread -- the hard t understand but very versatile thread
+
+
 }
+
